02fb8ef58cb0053888c4da75098e242520557644
Felipe Marques <contato@felipemarques.com.br>
2023-12-11 13:04:07 -0300

    working with realtime graph

---
+++ renderer/pages/components/BrushChart3.tsx
@@ -0,0 +1,76 @@
+import React, { useEffect, useState } from "react";
+import Chart from "react-apexcharts";
+
+const BrushChart3 = (props) => {
+  const [data, setData] = useState([]);
+
+  useEffect(() => {
+    const intervalId = setInterval(() => {
+      // Atualizar estado para manter apenas os últimos 50 pontos
+      setData(prevData => [...prevData.slice(-50), [new Date().getTime(), Math.floor(Math.random() * 100)]]);
+    }, 1000);
+
+    // Limpar o intervalo quando o componente for desmontado
+    return () => clearInterval(intervalId);
+  }, []);
+
+  const options = {
+    chart: {
+      id: "chart2",
+      type: "line",
+      height: 230,
+      toolbar: {
+        autoSelected: "pan",
+        show: false,
+      },
+    },
+    colors: ["#546E7A"],
+    stroke: {
+      width: 3,
+    },
+    dataLabels: {
+      enabled: false,
+    },
+    fill: {
+      opacity: 1,
+    },
+    markers: {
+      size: 1,
+    },
+    xaxis: {
+      type: "datetime",
+    },
+    animations: {
+        enabled: false,
+    },
+    annotations: {
+        xaxis: [
+          {
+            x: new Date().getTime(),
+            borderColor: "#00E396",
+            label: {
+              text: props.graphLabel || "GRÁFICO",
+              style: {
+                color: "#fff",
+                background: "#00E396",
+              },
+                offsetY: 0, // ajuste vertical
+                offsetX: 0, // ajuste horizontal
+            },
+          },
+        ],
+      },
+  };
+
+  const series = [{ data: data, animate: { enabled: false } }];
+
+  return (
+    <div id="wrapper">
+      <div id={"chart-line-" + props.chartID}>
+        <Chart options={options} series={series} type="line" height={230} />
+      </div>
+    </div>
+  );
+};
+
+export default BrushChart3;
