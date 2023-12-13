c48b9ce24d221472f3cf0b7b1349fe1812287bda
ChristoferMendes <christoferluizdsm@gmail.com>
2023-12-13 17:58:25 -0300

    feat: importing home-v2 inside home

--- renderer/pages/home.tsx
+++ renderer/pages/home.tsx
@@ -6,6 +6,7 @@ import Link from "next/link";
 import Image from "next/image";
 
 import dynamic from "next/dynamic";
+import { HomeV2 } from "./home-v2";
 
 const MyLineChart = dynamic(() => import("../components/MyLineChart"), {
   ssr: false,
@@ -38,198 +39,199 @@ export default function HomePage() {
         <title>Fan Control</title>
       </Head>
 
-      <div className={stylesTitleBar.titleBar}>
-        <div className={stylesTitleBar.dragContainer}></div>
+      <HomeV2 />
+      {/*<div className={stylesTitleBar.titleBar}>*/}
+      {/*  <div className={stylesTitleBar.dragContainer}></div>*/}
 
-        <div className={stylesTitleBar.buttons}>
-          <button id="minimize-btn" type="button">
-            -
-          </button>
-          <button id="close-btn" type="button">
-            ✕
-          </button>
-        </div>
-      </div>
+      {/*  <div className={stylesTitleBar.buttons}>*/}
+      {/*    <button id="minimize-btn" type="button">*/}
+      {/*      -*/}
+      {/*    </button>*/}
+      {/*    <button id="close-btn" type="button">*/}
+      {/*      ✕*/}
+      {/*    </button>*/}
+      {/*  </div>*/}
+      {/*</div>*/}
 
-      <div className="bg-gray-800">
-        <div
-          className={
-            stylesTitleBar.noDragWindow + " flex h-full flex-wrap gap-0 "
-          }
-        >
-          <div className={ stylesTitleBar.dragWindow + "h-12 flex text-white"}></div>
+      {/*<div className="bg-gray-800">*/}
+      {/*  <div*/}
+      {/*    className={*/}
+      {/*      stylesTitleBar.noDragWindow + " flex h-full flex-wrap gap-0 "*/}
+      {/*    }*/}
+      {/*  >*/}
+      {/*    <div className={ stylesTitleBar.dragWindow + "h-12 flex text-white"}></div>*/}
 
-          <div
-            className={
-              stylesTitleBar.dragWindow +
-              " h-12 flex-auto bg-gray-800 justify-center text-center text-white"
-            }
-          ></div>
+      {/*    <div*/}
+      {/*      className={*/}
+      {/*        stylesTitleBar.dragWindow +*/}
+      {/*        " h-12 flex-auto bg-gray-800 justify-center text-center text-white"*/}
+      {/*      }*/}
+      {/*    ></div>*/}
 
-          <div className="h-12 flex text-right text-white">
-            <Image
-              width="100px"
-              height="50px"
-              src="/Acer-Logo.png"
-              className={stylesTitleBar.dragWindow + ""}
-              style={{ marginLeft: "10px" }}
-            ></Image>
+      {/*    <div className="h-12 flex text-right text-white">*/}
+      {/*      <Image*/}
+      {/*        width="100px"*/}
+      {/*        height="50px"*/}
+      {/*        src="/Acer-Logo.png"*/}
+      {/*        className={stylesTitleBar.dragWindow + ""}*/}
+      {/*        style={{ marginLeft: "10px" }}*/}
+      {/*      ></Image>*/}
 
-            <Image
-              width="100px"
-              height="50px"
-              src="/acer-nitrosense.webp"
-              className={stylesTitleBar.dragWindow + ""}
-            ></Image>
+      {/*      <Image*/}
+      {/*        width="100px"*/}
+      {/*        height="50px"*/}
+      {/*        src="/acer-nitrosense.webp"*/}
+      {/*        className={stylesTitleBar.dragWindow + ""}*/}
+      {/*      ></Image>*/}
 
-            <Image
-              width="180px"
-              height="100px"
-              src="/nvidia-geforce-gtx-logo.png"
-              className={stylesTitleBar.dragWindow + ""}
-            ></Image>
+      {/*      <Image*/}
+      {/*        width="180px"*/}
+      {/*        height="100px"*/}
+      {/*        src="/nvidia-geforce-gtx-logo.png"*/}
+      {/*        className={stylesTitleBar.dragWindow + ""}*/}
+      {/*      ></Image>*/}
 
-            <button
-              type="button"
-              onClick={onClickMinimize}
-              className={
-                "dark:shadow-highlight/20 relative w-20 flex-auto bg-gray-500 px-3 py-1.5 text-lg font-semibold leading-6 text-white shadow-sm hover:bg-gray-400"
-              }
-            >
-              -
-            </button>
+      {/*      <button*/}
+      {/*        type="button"*/}
+      {/*        onClick={onClickMinimize}*/}
+      {/*        className={*/}
+      {/*          "dark:shadow-highlight/20 relative w-20 flex-auto bg-gray-500 px-3 py-1.5 text-lg font-semibold leading-6 text-white shadow-sm hover:bg-gray-400"*/}
+      {/*        }*/}
+      {/*      >*/}
+      {/*        -*/}
+      {/*      </button>*/}
 
-            <button
-              type="button"
-              onClick={onClickClose}
-              className="dark:shadow-highlight/20 relative w-20 flex-auto bg-gray-500 px-3 py-1.5 text-lg font-semibold leading-6 text-white shadow-sm hover:bg-gray-400"
-            >
-              x
-            </button>
-          </div>
-        </div>
+      {/*      <button*/}
+      {/*        type="button"*/}
+      {/*        onClick={onClickClose}*/}
+      {/*        className="dark:shadow-highlight/20 relative w-20 flex-auto bg-gray-500 px-3 py-1.5 text-lg font-semibold leading-6 text-white shadow-sm hover:bg-gray-400"*/}
+      {/*      >*/}
+      {/*        x*/}
+      {/*      </button>*/}
+      {/*    </div>*/}
+      {/*  </div>*/}
 
-        <div className="flex h-full flex-wrap gap-2">
-          <div className="w-60 flex-2 h-auto flex-col text-white">
-            <div>FAN CONTROL</div>
+      {/*  <div className="flex h-full flex-wrap gap-2">*/}
+      {/*    <div className="w-60 flex-2 h-auto flex-col text-white">*/}
+      {/*      <div>FAN CONTROL</div>*/}
 
-            <div>
-              <button
-                type="button"
-                className="dark:shadow-highlight/20 relative w-full flex-auto bg-gray-500 px-3 py-1.5 text-sm font-semibold leading-6 text-white shadow-sm hover:bg-gray-400"
-              >
-                Auto
-              </button>
-            </div>
+      {/*      <div>*/}
+      {/*        <button*/}
+      {/*          type="button"*/}
+      {/*          className="dark:shadow-highlight/20 relative w-full flex-auto bg-gray-500 px-3 py-1.5 text-sm font-semibold leading-6 text-white shadow-sm hover:bg-gray-400"*/}
+      {/*        >*/}
+      {/*          Auto*/}
+      {/*        </button>*/}
+      {/*      </div>*/}
 
-            <div>
-              <button
-                type="button"
-                className="dark:shadow-highlight/20 relative w-full flex-auto bg-gray-500 px-3 py-1.5 text-sm font-semibold leading-6 text-white shadow-sm hover:bg-gray-400"
-              >
-                Max
-              </button>
-            </div>
+      {/*      <div>*/}
+      {/*        <button*/}
+      {/*          type="button"*/}
+      {/*          className="dark:shadow-highlight/20 relative w-full flex-auto bg-gray-500 px-3 py-1.5 text-sm font-semibold leading-6 text-white shadow-sm hover:bg-gray-400"*/}
+      {/*        >*/}
+      {/*          Max*/}
+      {/*        </button>*/}
+      {/*      </div>*/}
 
-            <div>
-              <button
-                type="button"
-                className="dark:shadow-highlight/20 relative w-full flex-auto bg-gray-500 px-3 py-1.5 text-sm font-semibold leading-6 text-white shadow-sm hover:bg-gray-400"
-              >
-                Custom
-              </button>
-            </div>
+      {/*      <div>*/}
+      {/*        <button*/}
+      {/*          type="button"*/}
+      {/*          className="dark:shadow-highlight/20 relative w-full flex-auto bg-gray-500 px-3 py-1.5 text-sm font-semibold leading-6 text-white shadow-sm hover:bg-gray-400"*/}
+      {/*        >*/}
+      {/*          Custom*/}
+      {/*        </button>*/}
+      {/*      </div>*/}
 
-            <div className="invisible">
-              <button className="focus:shadow-outline h-12 w-full bg-indigo-700 px-3 text-indigo-100 transition-colors duration-150 hover:bg-indigo-800">
-                Large block button
-              </button>
-            </div>
+      {/*      <div className="invisible">*/}
+      {/*        <button className="focus:shadow-outline h-12 w-full bg-indigo-700 px-3 text-indigo-100 transition-colors duration-150 hover:bg-indigo-800">*/}
+      {/*          Large block button*/}
+      {/*        </button>*/}
+      {/*      </div>*/}
 
-            <div className="invisible">
-              <button className="focus:shadow-outline h-12 w-full bg-indigo-700 px-3 text-indigo-100 transition-colors duration-150 hover:bg-indigo-800">
-                Large block button
-              </button>
-            </div>
-          </div>
+      {/*      <div className="invisible">*/}
+      {/*        <button className="focus:shadow-outline h-12 w-full bg-indigo-700 px-3 text-indigo-100 transition-colors duration-150 hover:bg-indigo-800">*/}
+      {/*          Large block button*/}
+      {/*        </button>*/}
+      {/*      </div>*/}
+      {/*    </div>*/}
 
-          <div className="h-auto flex-1 flex-wrap text-white">
-            <div className="flex flex-wrap gap-10">
-              <div className="h-auto flex-auto flex-col text-white">
-                <div>CPU</div>
+      {/*    <div className="h-auto flex-1 flex-wrap text-white">*/}
+      {/*      <div className="flex flex-wrap gap-10">*/}
+      {/*        <div className="h-auto flex-auto flex-col text-white">*/}
+      {/*          <div>CPU</div>*/}
 
-                <div>
-                  <div className={styles.circleFan}>5882 RPM</div>
+      {/*          <div>*/}
+      {/*            <div className={styles.circleFan}>5882 RPM</div>*/}
 
-                  <Image
-                    className={styles.fan + " ml-auto mr-auto"}
-                    src="/fan3.png"
-                    alt="fan"
-                    width="256px"
-                    height="256px"
-                  />
-                </div>
-              </div>
+      {/*            <Image*/}
+      {/*              className={styles.fan + " ml-auto mr-auto"}*/}
+      {/*              src="/fan3.png"*/}
+      {/*              alt="fan"*/}
+      {/*              width="256px"*/}
+      {/*              height="256px"*/}
+      {/*            />*/}
+      {/*          </div>*/}
+      {/*        </div>*/}
 
-              <div className="h-auto flex-auto flex-col text-white">
-                <div>GPU</div>
+      {/*        <div className="h-auto flex-auto flex-col text-white">*/}
+      {/*          <div>GPU</div>*/}
 
-                <div>
-                  <div className={styles.circleFan}>6000 RPM</div>
+      {/*          <div>*/}
+      {/*            <div className={styles.circleFan}>6000 RPM</div>*/}
 
-                  <Image
-                    className={styles.fan + " ml-auto mr-auto"}
-                    src="/fan3.png"
-                    alt="fan"
-                    width="256px"
-                    height="256px"
-                  />
-                </div>
-              </div>
-            </div>
-          </div>
-        </div>
+      {/*            <Image*/}
+      {/*              className={styles.fan + " ml-auto mr-auto"}*/}
+      {/*              src="/fan3.png"*/}
+      {/*              alt="fan"*/}
+      {/*              width="256px"*/}
+      {/*              height="256px"*/}
+      {/*            />*/}
+      {/*          </div>*/}
+      {/*        </div>*/}
+      {/*      </div>*/}
+      {/*    </div>*/}
+      {/*  </div>*/}
 
-        <div className="flex h-auto flex-wrap gap-2">
-          <div className="w-60 flex-2 h-auto flex-col text-white">
-            <div>POWER PLAN</div>
+      {/*  <div className="flex h-auto flex-wrap gap-2">*/}
+      {/*    <div className="w-60 flex-2 h-auto flex-col text-white">*/}
+      {/*      <div>POWER PLAN</div>*/}
 
-            <div>
-              <button
-                type="button"
-                className="dark:shadow-highlight/20 relative w-full flex-auto bg-gray-500 px-3 py-1.5 text-sm font-semibold leading-6 text-white shadow-sm hover:bg-gray-400"
-              >
-                Power Save
-              </button>
-            </div>
+      {/*      <div>*/}
+      {/*        <button*/}
+      {/*          type="button"*/}
+      {/*          className="dark:shadow-highlight/20 relative w-full flex-auto bg-gray-500 px-3 py-1.5 text-sm font-semibold leading-6 text-white shadow-sm hover:bg-gray-400"*/}
+      {/*        >*/}
+      {/*          Power Save*/}
+      {/*        </button>*/}
+      {/*      </div>*/}
 
-            <div>
-              <button
-                type="button"
-                className="dark:shadow-highlight/20 relative w-full flex-auto bg-gray-500 px-3 py-1.5 text-sm font-semibold leading-6 text-white shadow-sm hover:bg-gray-400"
-              >
-                Balance
-              </button>
-            </div>
+      {/*      <div>*/}
+      {/*        <button*/}
+      {/*          type="button"*/}
+      {/*          className="dark:shadow-highlight/20 relative w-full flex-auto bg-gray-500 px-3 py-1.5 text-sm font-semibold leading-6 text-white shadow-sm hover:bg-gray-400"*/}
+      {/*        >*/}
+      {/*          Balance*/}
+      {/*        </button>*/}
+      {/*      </div>*/}
 
-            <div>
-              <button
-                type="button"
-                className="dark:shadow-highlight/20 relative w-full flex-auto bg-gray-500 px-3 py-1.5 text-sm font-semibold leading-6 text-white shadow-sm hover:bg-gray-400"
-              >
-                High-Performance
-              </button>
-            </div>
-          </div>
+      {/*      <div>*/}
+      {/*        <button*/}
+      {/*          type="button"*/}
+      {/*          className="dark:shadow-highlight/20 relative w-full flex-auto bg-gray-500 px-3 py-1.5 text-sm font-semibold leading-6 text-white shadow-sm hover:bg-gray-400"*/}
+      {/*        >*/}
+      {/*          High-Performance*/}
+      {/*        </button>*/}
+      {/*      </div>*/}
+      {/*    </div>*/}
 
-          <div className="h-auto flex-1 flex-wrap bg-white">
-            <div className="w-full flex-auto h-auto flex-col text-white">
-              <MyLineChart chartID="cpu" graphLabel="CPU"></MyLineChart>
-              <MyLineChart chartID="gpu" graphLabel="GPU"></MyLineChart>
-            </div>
-          </div>
-        </div>
-      </div>
+      {/*    <div className="h-auto flex-1 flex-wrap bg-white">*/}
+      {/*      <div className="w-full flex-auto h-auto flex-col text-white">*/}
+      {/*        <MyLineChart chartID="cpu" graphLabel="CPU"></MyLineChart>*/}
+      {/*        <MyLineChart chartID="gpu" graphLabel="GPU"></MyLineChart>*/}
+      {/*      </div>*/}
+      {/*    </div>*/}
+      {/*  </div>*/}
+      {/*</div>*/}
     </React.Fragment>
   );
 }
