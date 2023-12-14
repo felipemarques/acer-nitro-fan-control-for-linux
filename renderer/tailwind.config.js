e84128105861e7f2590b4c70298daa3f222d2d43
ChristoferMendes <christoferluizdsm@gmail.com>
2023-12-14 13:53:17 -0300

    style: add slow and slower spin animations

--- renderer/tailwind.config.js
+++ renderer/tailwind.config.js
@@ -6,7 +6,12 @@ module.exports = {
     './renderer/components/**/*.{js,ts,jsx,tsx}',
   ],
   theme: {
-    extend: {},
+    extend: {
+      animation: {
+        'spin-slow': 'spin 3s linear infinite',
+        'spin-slower': 'spin 5s linear infinite',
+      }
+    },
   },
   plugins: [],
 }
